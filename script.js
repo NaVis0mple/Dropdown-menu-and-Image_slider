9ba08b2cedf6a069b1f9277f94855b97945167a9
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-28 18:47:14 +0800

    add img but logic wrong

--- script.js
+++ script.js
@@ -42,3 +42,58 @@ createDropDownMenu()
 createDropDownMenu()
 createDropDownMenu()
 document.querySelector('.try').appendChild(nav)
+
+
+//image slider
+const pic1 = document.querySelector('.pic-1')
+pic1.classList.add('hide')
+const pic2 = document.querySelector('.pic-2')
+pic2.classList.add('hide')
+const pic3 = document.querySelector('.pic-3')
+pic3.classList.add('hide')
+const pic4 = document.querySelector('.pic-4')
+pic4.classList.add('hide')
+const pic5 = document.querySelector('.pic-5')
+pic5.classList.add('hide')
+
+let picStatus = '1'
+let picLength = '5'
+function changePic(){
+    let targetPic = `pic-${parseInt(picStatus)}`
+    const getTargetPic = document.querySelector(`.${targetPic}`)  
+    if(picStatus ==='6'){
+        picStatus='1'
+    }
+    let previousPic
+    if (targetPic ==='pic-1'){
+        previousPic ='pic-5'
+    }else {
+        previousPic = `pic-${parseInt(picStatus)-1}`
+    }
+    const getPreviousPic = document.querySelector(`.${previousPic}`)
+
+    console.log(picStatus)
+    console.log(targetPic)
+    console.log(previousPic)
+
+    getTargetPic.classList.remove('hide')
+    getPreviousPic.classList.add('hide')
+    picStatus =` ${parseInt(picStatus) + 1}`
+    console.log(picStatus)
+
+    if(picStatus ==='6'){
+        picStatus='1'
+    }
+    console.log(picStatus)
+    previousPic = ''
+    console.log(previousPic)
+
+}
+
+changePic()
+changePic()
+changePic()
+changePic()
+changePic()
+changePic()
+
