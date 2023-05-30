1d238bfab85de736efbdf1cd59d15e4df0e90b4d
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-30 23:49:19 +0800

    use pure html and css to build slider

--- script.js
+++ script.js
@@ -45,58 +45,60 @@ document.querySelector('.try').appendChild(nav)
 
 
 //image slider
-const pic1 = document.querySelector('.pic-1')
-const pic2 = document.querySelector('.pic-2')
-pic2.classList.add('hide')
-const pic3 = document.querySelector('.pic-3')
-pic3.classList.add('hide')
-const pic4 = document.querySelector('.pic-4')
-pic4.classList.add('hide')
-const pic5 = document.querySelector('.pic-5')
-pic5.classList.add('hide')
+// const pic1 = document.querySelector('.pic-1')
+// const pic2 = document.querySelector('.pic-2')
+// pic2.classList.add('hide')
+// const pic3 = document.querySelector('.pic-3')
+// pic3.classList.add('hide')
+// const pic4 = document.querySelector('.pic-4')
+// pic4.classList.add('hide')
+// const pic5 = document.querySelector('.pic-5')
+// pic5.classList.add('hide')
 
-let picStatus = '1'
-let picLength = '5'
+// let picStatus = 1
+// let picLength = 5
 
 
-function changePic(){
-    picStatus =`${parseInt(picStatus) + 1}`
-    if (picStatus === `${parseInt(picLength)+1}`){
-        picStatus = '1'
-    }
-    let targetPic = `pic-${parseInt(picStatus)}`
-    let previousPic = `pic-${parseInt(picStatus)-1}`
-    if (previousPic === 'pic-0'){
-        previousPic = `pic-${parseInt(picLength)}`
-    }
-    const getTargetPic = document.querySelector(`.${targetPic}`)  
-    const getPreviousPic = document.querySelector(`.${previousPic}`)
+// function changePic(){
+//     picStatus = picStatus + 1
+//     if (picStatus === picLength+1){
+//         picStatus = 1
+//     }
+//     let targetPic = `pic-${picStatus}`
+//     let previousPic = `pic-${picStatus-1}`
+//     if (previousPic === 'pic-0'){
+//         previousPic = `pic-${picLength}`
+//     }
+//     const getTargetPic = document.querySelector(`.${targetPic}`)  
+//     const getPreviousPic = document.querySelector(`.${previousPic}`)
 
-    getTargetPic.classList.remove('hide')
-    getPreviousPic.classList.add('hide')
-}
+//     getTargetPic.classList.remove('hide')
+//     getPreviousPic.classList.add('hide')
+// }
 
-function changePicBack (){
-    picStatus =`${parseInt(picStatus) - 1}`
-    if (picStatus==='0'){
-        picStatus = `${parseInt(picLength)}`
-    }
-    let targetPic = `pic-${parseInt(picStatus)}`
-    let previousPic = `pic-${parseInt(picStatus)+1}`
-    if (previousPic===`pic-${parseInt(picLength)+1}`){
-        previousPic = 'pic-1'
-    }
-    const getTargetPic = document.querySelector(`.${targetPic}`)  
-    const getPreviousPic = document.querySelector(`.${previousPic}`)
-    getTargetPic.classList.remove('hide')
-    getPreviousPic.classList.add('hide')
-}
+// function changePicBack (){
+//     picStatus = picStatus - 1
+//     if (picStatus===0){
+//         picStatus = picLength
+//     }
+//     let targetPic = `pic-${picStatus}`
+//     let previousPic = `pic-${picStatus+1}`
+//     if (previousPic===`pic-${picLength+1}`){
+//         previousPic = 'pic-1'
+//     }
+//     const getTargetPic = document.querySelector(`.${targetPic}`)  
+//     const getPreviousPic = document.querySelector(`.${previousPic}`)
+//     getTargetPic.classList.remove('hide')
+//     getPreviousPic.classList.add('hide')
+// }
 
-const buttonForward = document.querySelector('.forward')
-buttonForward.addEventListener('click',()=>{
-    changePic()
-})
-const buttonBackward = document.querySelector('.back')
-buttonBackward.addEventListener('click',()=>{
-    changePicBack()
-})
+// const buttonForward = document.querySelector('.forward')
+// buttonForward.addEventListener('click',()=>{
+//     changePic()
+// })
+// const buttonBackward = document.querySelector('.back')
+// buttonBackward.addEventListener('click',()=>{
+//     changePicBack()
+// })
+
+const match = 
