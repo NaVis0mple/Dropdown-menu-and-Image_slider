41ebb1dae81a300d4dd6fb639cda80486047bb7d
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-06-02 16:14:29 +0800

    refactor

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 const nav = document.createElement('nav')
 function createDropDownMenu () {
-    
-    
-    const start = document.createElement('div')
-    start.classList.add('start')
-    start.innerHTML = 'start'
-    nav.appendChild(start)
+  const start = document.createElement('div')
+  start.classList.add('start')
+  start.innerHTML = 'start'
+  nav.appendChild(start)
 
-    const togglepart = document.createElement('div')
-    togglepart.classList.add('togglePart')
-    start.appendChild(togglepart)
+  const togglepart = document.createElement('div')
+  togglepart.classList.add('togglePart')
+  start.appendChild(togglepart)
 
-    
-    const gridcontainer = document.createElement('div')
-    gridcontainer.classList.add('gridcontainer')
-    togglepart.appendChild(gridcontainer)
+  const gridcontainer = document.createElement('div')
+  gridcontainer.classList.add('gridcontainer')
+  togglepart.appendChild(gridcontainer)
 
-    const itemlist = ['a','b','c','d','e']
-    itemlist.forEach(item=>{
-        const createitem = document.createElement('div')   
-        createitem.classList.add('item')
-        gridcontainer.appendChild(createitem)    
-
-    })
-    
+  const itemlist = ['a', 'b', 'c', 'd', 'e']
+  itemlist.forEach(item => {
+    const createitem = document.createElement('div')
+    createitem.classList.add('item')
+    gridcontainer.appendChild(createitem)
+  })
 }
 createDropDownMenu()
 createDropDownMenu()
@@ -43,174 +38,118 @@ createDropDownMenu()
 createDropDownMenu()
 document.querySelector('.try').appendChild(nav)
 
-
 //image slider
-// const pic1 = document.querySelector('.pic-1')
-// const pic2 = document.querySelector('.pic-2')
-// pic2.classList.add('hide')
-// const pic3 = document.querySelector('.pic-3')
-// pic3.classList.add('hide')
-// const pic4 = document.querySelector('.pic-4')
-// pic4.classList.add('hide')
-// const pic5 = document.querySelector('.pic-5')
-// pic5.classList.add('hide')
 
-// let picStatus = 1
-// let picLength = 5
-
-
-// function changePic(){
-//     picStatus = picStatus + 1
-//     if (picStatus === picLength+1){
-//         picStatus = 1
-//     }
-//     let targetPic = `pic-${picStatus}`
-//     let previousPic = `pic-${picStatus-1}`
-//     if (previousPic === 'pic-0'){
-//         previousPic = `pic-${picLength}`
-//     }
-//     const getTargetPic = document.querySelector(`.${targetPic}`)  
-//     const getPreviousPic = document.querySelector(`.${previousPic}`)
-
-//     getTargetPic.classList.remove('hide')
-//     getPreviousPic.classList.add('hide')
-// }
-
-// function changePicBack (){
-//     picStatus = picStatus - 1
-//     if (picStatus===0){
-//         picStatus = picLength
-//     }
-//     let targetPic = `pic-${picStatus}`
-//     let previousPic = `pic-${picStatus+1}`
-//     if (previousPic===`pic-${picLength+1}`){
-//         previousPic = 'pic-1'
-//     }
-//     const getTargetPic = document.querySelector(`.${targetPic}`)  
-//     const getPreviousPic = document.querySelector(`.${previousPic}`)
-//     getTargetPic.classList.remove('hide')
-//     getPreviousPic.classList.add('hide')
-// }
-
-// const buttonForward = document.querySelector('.forward')
-// buttonForward.addEventListener('click',()=>{
-//     changePic()
-// })
-// const buttonBackward = document.querySelector('.back')
-// buttonBackward.addEventListener('click',()=>{
-//     changePicBack()
-// })
 const anchor = document.querySelectorAll('a')
-anchor.forEach(a=>{  
-    a.addEventListener('click',()=>{
-        setTimeout(() => {
-            const hash = getHash()
-            console.log(hash) 
-        }, 0)  
-        anchor.forEach(other=>{
-            if(other!==a){
-                other.style.backgroundColor= ''
-            }
-        })
-        a.style.backgroundColor = 'black'
+anchor.forEach(a => {
+  a.addEventListener('click', () => {
+    clearInterval(timer)
+    timeFiveSec()
+    setTimeout(() => {
+      const hash = getHash()
+      console.log(hash)
+    }, 0)
+    anchor.forEach(other => {
+      if (other !== a) {
+        other.style.backgroundColor = ''
+      }
     })
+    a.style.backgroundColor = 'black'
+  })
 })
 
 const rightbutton = document.getElementById('right')
 const leftbutton = document.getElementById('left')
 
-function updateHash(newHash) {
-    window.location.hash = newHash;
+function updateHash (newHash) {
+  window.location.hash = newHash
 }
-function getHash (){
-    const hash = document.location.hash
-    return hash
+function getHash () {
+  const hash = document.location.hash
+  return hash
 }
-function handleHashF(){
+function handleHashF () {
+  const hash = getHash()
+  const indexpic = hash.slice(-1)
+  let nextpic = parseInt(indexpic) + 1
+  if (nextpic === 6) {
+    nextpic = 1
+  }
+  return nextpic
+}
+function handleHashB () {
+  const hash = getHash()
+  const indexpic = hash.slice(-1)
+  let nextpic = parseInt(indexpic) - 1
+  if (nextpic === 0) {
+    nextpic = 5
+  }
+  return nextpic
+}
+
+rightbutton.addEventListener('click', () => {
+  clearInterval(timer)
+  timeFiveSec()
+  const nextpic = handleHashF()
+  const pic = document.getElementById(`pic-${nextpic}`)
+  pic.scrollIntoView({ behavior: 'smooth' })
+  updateHash(`#pic-${nextpic}`)
+
+  anchor.forEach(a => {
     const hash = getHash()
-    const indexpic = hash.slice(-1)
-    let nextpic = parseInt(indexpic) +1
-    if (nextpic===6){
-        nextpic = 1
+    if (a.hash !== hash) {
+      a.style.backgroundColor = ''
+    } else {
+      a.style.backgroundColor = 'black'
     }
-    return nextpic
-}
-function handleHashB(){
+  })
+})
+
+leftbutton.addEventListener('click', () => {
+  clearInterval(timer)
+  timeFiveSec()
+  const nextpic = handleHashB()
+  const pic = document.getElementById(`pic-${nextpic}`)
+  pic.scrollIntoView({ behavior: 'smooth' })
+  updateHash(`#pic-${nextpic}`)
+
+  anchor.forEach(a => {
     const hash = getHash()
-    const indexpic = hash.slice(-1)
-    let nextpic = parseInt(indexpic) -1
-    if (nextpic===0){
-        nextpic = 5
+    if (a.hash !== hash) {
+      a.style.backgroundColor = ''
+    } else {
+      a.style.backgroundColor = 'black'
     }
-    return nextpic
-}
-
-
-
-
-rightbutton.addEventListener('click',()=>{
-    clearInterval(timer)
-    timeFiveSec ()
+  })
+})
+let timer
+function timeFiveSec () {
+  clearInterval(timer)
+  timer = setInterval(() => {
     const nextpic = handleHashF()
     const pic = document.getElementById(`pic-${nextpic}`)
     pic.scrollIntoView({ behavior: 'smooth' })
     updateHash(`#pic-${nextpic}`)
-    
-    anchor.forEach(a=>{
-        const hash = getHash()
-        if(a.hash!==hash){
-            a.style.backgroundColor= ''
-        }else {
-            a.style.backgroundColor = 'black'
-        }
-    })
-})
 
-leftbutton.addEventListener('click',()=>{
-    clearInterval(timer)
-    timeFiveSec ()
-    const nextpic = handleHashB()
-    const pic = document.getElementById(`pic-${nextpic}`)
-    pic.scrollIntoView({ behavior: 'smooth' })
-    updateHash(`#pic-${nextpic}`)
-
-    anchor.forEach(a=>{
-        const hash = getHash()
-        if(a.hash!==hash){
-            a.style.backgroundColor= ''
-        }else {
-            a.style.backgroundColor = 'black'
-        }
+    anchor.forEach(a => {
+      const hash = getHash()
+      if (a.hash !== hash) {
+        a.style.backgroundColor = ''
+      } else {
+        a.style.backgroundColor = 'black'
+      }
     })
-})
-let timer
-function timeFiveSec ()  {
-    clearInterval(timer)
-    timer = setInterval(() => {
-        const nextpic = handleHashF()
-        const pic = document.getElementById(`pic-${nextpic}`)
-        pic.scrollIntoView({ behavior: 'smooth' })
-        updateHash(`#pic-${nextpic}`)
-    
-        anchor.forEach(a=>{
-            const hash = getHash()
-            if(a.hash!==hash){
-            a.style.backgroundColor= ''
-        }else {
-            a.style.backgroundColor = 'black'
-        }
-    })
-    }, 3000);
+  }, 3000)
 }
 
 //flow
 updateHash(`#pic-1`)
-anchor.forEach(a=>{
-    const hash = getHash()
-    if(a.hash!==hash){
-        a.style.backgroundColor= ''
-    }else {
-        a.style.backgroundColor = 'black'
-    }
+anchor.forEach(a => {
+  const hash = getHash()
+  if (a.hash !== hash) {
+    a.style.backgroundColor = ''
+  } else {
+    a.style.backgroundColor = 'black'
+  }
 })
 timeFiveSec()
